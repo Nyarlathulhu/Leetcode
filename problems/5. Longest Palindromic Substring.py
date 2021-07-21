6237f1228f5aab1e373b013429f09048ec2b2810
Nyarlathulhu <49614628+Nyarlathulhu@users.noreply.github.com>
2021-07-21 11:36:09 +0800

    Create 5. Longest Palindromic Substring.py

---
+++ problems/5. Longest Palindromic Substring.py
@@ -0,0 +1,23 @@
+# RELATED TOPICS:
+# string | dynamic programming
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        s = '#'+'#'.join(s)+'#'
+        pos = maxright = 0
+        RL = [0]*len(s)
+        maxcenter = 0
+        
+        for i in range(len(s)):
+            if i<maxright:
+                RL[i] = min(maxright-i, RL[pos*2-i] )
+            else:
+                RL[i] = 0
+            while i-RL[i]-1>=0 and i+RL[i]+1<len(s) and s[i-RL[i]-1] == s[i+RL[i]+1]:
+                RL[i] += 1
+            if i+RL[i] > maxright:
+                maxright = RL[i] + i
+                pos = i
+            if RL[i] > RL[maxcenter]:
+                maxcenter = i
+        return s[maxcenter-RL[maxcenter]:maxcenter+RL[maxcenter]+1].replace('#','')
