20c194fc207557cfa94c8c1ecb917d45f72c77e2
Nyarlathulhu <49614628+Nyarlathulhu@users.noreply.github.com>
2021-07-22 20:25:53 +0800

    Create 0901_Online_Stock_Span.py

---
+++ problems/0901_Online_Stock_Span.py
@@ -0,0 +1,30 @@
+# RELATED TOPICS:
+# Stack | Design | Monotonic Stack | Data Stream
+
+from collections import deque
+
+
+class StockSpanner:
+
+    def __init__(self):
+        self.mono_stack = deque()
+        self.span = -1
+        
+
+    def next(self, price: int) -> int:
+        self.span += 1
+        if not self.mono_stack or self.mono_stack[-1][1] > price:
+            self.mono_stack.append([self.span, price])
+        else:
+            while self.mono_stack and self.mono_stack[-1][1] <= price:
+                self.mono_stack.pop()
+            self.mono_stack.append([self.span, price])
+        
+        return self.span + 1 if len(self.mono_stack) == 1 else self.span - self.mono_stack[-2][0]
+            
+        
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
