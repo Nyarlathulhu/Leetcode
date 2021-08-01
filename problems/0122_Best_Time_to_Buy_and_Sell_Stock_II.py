3a908175c8646556d77a84b11f23b203d00e0943
Nyarlathulhu <49614628+Nyarlathulhu@users.noreply.github.com>
2021-08-01 22:21:46 +0800

    Update 0122_Best_Time_to_Buy_and_Sell_Stock_II.py

--- problems/0122_Best_Time_to_Buy_and_Sell_Stock_II.py
+++ problems/0122_Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -5,9 +5,9 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         income_i_0 = 0
         income_i_1 = -10001
-        for i in range(len(prices)):
+        for price in prices:
             temp = income_i_0
-            income_i_0 = max(income_i_0, income_i_1 + prices[i])
-            income_i_1 = max(income_i_1, temp - prices[i])
+            income_i_0 = max(income_i_0, income_i_1 + price)
+            income_i_1 = max(income_i_1, temp - price)
         return income_i_0
     
