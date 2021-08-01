e58d72a3c24d7a5f8db208cbd9a8975addbbb413
Nyarlathulhu <49614628+Nyarlathulhu@users.noreply.github.com>
2021-08-01 22:22:19 +0800

    Update 0121_Best_Time_to_Buy_and_Sell_Stock.py

--- problems/0121_Best_Time_to_Buy_and_Sell_Stock.py
+++ problems/0121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -7,8 +7,8 @@ class Solution:
         # 0: no stock yesterday, 1: bought stock yesterday
         income_i_0 = 0
         income_i_1 = -10001
-        for i in range(len(prices)):
-            income_i_0 = max(income_i_0, income_i_1 + prices[i])
-            income_i_1 = max(income_i_1, -prices[i])
+        for price in prices:
+            income_i_0 = max(income_i_0, income_i_1 + price)
+            income_i_1 = max(income_i_1, -price)
         return income_i_0
     
