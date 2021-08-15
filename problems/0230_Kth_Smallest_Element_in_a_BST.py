dc596724b71f2b318365519d6264b67abc8e1ed8
Nyarlathulhu <49614628+Nyarlathulhu@users.noreply.github.com>
2021-08-15 22:09:54 +0800

    Create 0230_Kth_Smallest_Element_in_a_BST.py

---
+++ problems/0230_Kth_Smallest_Element_in_a_BST.py
@@ -0,0 +1,22 @@
+# RELATED TOPICS:
+# Tree | Depth-First Search | Breadth-First Search | Binary Tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        nodes = []
+        while True:
+            while root:
+                nodes.append(root)
+                root = root.left
+            root = nodes.pop()
+            k -= 1
+            if k == 0:
+                return root.val
+            root = root.right
+        
