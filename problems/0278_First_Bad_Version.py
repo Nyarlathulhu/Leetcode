7900ee2f7aacbbcbeaef368223b06513d95d166a
Hengrui Shi <shihengrui42@gmail.com>
2022-03-07 16:25:24 -0500

    Create 0278_First_Bad_Version.py

---
+++ problems/0278_First_Bad_Version.py
@@ -0,0 +1,15 @@
+# RELATED TOPICS:
+# Binary Search | Interactive
+
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
