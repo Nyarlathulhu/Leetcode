4a288ab095c525a691b05c2f718033c8e2784a04
Hengrui Shi <shihengrui42@gmail.com>
2022-03-12 16:13:36 -0500

    Update 0234_Palindrome_Linked_List.py

--- problems/0234_Palindrome_Linked_List.py
+++ problems/0234_Palindrome_Linked_List.py
@@ -25,8 +25,6 @@ class Solution:
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        if fast:
-            slow = slow.next
         
         left = head
         right = reverse(slow)
